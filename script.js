01496250c31f235fa46c8f0efed1efec77361804
Yarcrazy <yacrazy@yandex.ru>
2020-04-12 11:05:56 +0700

    сделал переключатель убытки прибыль

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 "use strict";
 
-$('#drop-button').on('click', (e) => {
+$('#drop-button').on('click', () => {
   $('.hidden-form').toggleClass('hide');
   $('.arr').toggleClass('arr-down arr-right');
+});
+
+$('.limit-check').click((e) => {
+  let input = e.target.dataset.input;
+  $('#' + input).prop('disabled', (i, old) => {
+    return !old;
+  });
 });
